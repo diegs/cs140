c88d13fcb8d0e61d53bf930dac3ef662690d96cb
Evelyn Gillie <egillie@gmail.com>
2012-01-20 19:51:32 -0800

    lock waiters are now an ordered list

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered(&sema->waiters, &thread_current ()->elem,
+  cmp_thread_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -295,7 +296,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered (&cond->waiters, &waiter.elem, cmp_thread_priority, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
