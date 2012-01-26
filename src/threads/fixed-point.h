c8840048ee02b97725b754399bc4559caa5dde7a
Peter Thien Tan Pham <ptpham@myth10.Stanford.EDU>
2012-01-26 01:56:56 -0800

    Fixed point corrected

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -3,36 +3,41 @@
 #ifndef __FIXED_POINT_H__
 #define __FIXED_POINT_H__
 
-#include <math.h>
 #include <stdint.h>
 
-static const int32_t fp_f = 16384;
+/* This represents 14 bits of sub-integer precision */
+#define FP_PRECISION 16384
 
-static inline int32_t int2fp(int32_t num) {
-  return num*fp_f;
+
+typedef int32_t fp_t;
+
+static inline fp_t int2fp(int32_t num) {
+  return num*FP_PRECISION;
 }
 
-static inline int32_t fp2int(int32_t fp) {
-  return num/fp_f;
+static inline int32_t fp2int(fp_t fp) {
+  return fp/FP_PRECISION;
 }
 
-static inline int32_t fpadd(int32_t first, int32_t second) {
+static inline fp_t fpadd(fp_t first, fp_t second) {
   return first + second;
 }
 
-static inline int32_t fpsub(int32_t first, int32_t second) {
+static inline fp_t fpsub(fp_t first, fp_t second) {
   return first - second;
 }
 
-static inline int32_t fpmul(int32_t first, int32_t second) {
-  int64_t product = ((int64_t)first)*second/fp_f;
+static inline fp_t fpmul(fp_t first, fp_t second) {
+  int64_t product = ((int64_t)first)*second/FP_PRECISION;
   return product;
 }
 
-static inline int32_t fpdiv(int32_t first, int32_t second) {
-  int64_t quotient = ((int64_t)first)*fp_f/second;
+static inline fp_t fpdiv(fp_t first, fp_t second) {
+  int64_t quotient = ((int64_t)first)*FP_PRECISION/second;
   return quotient;
 }
+
+
 
 #endif
 
