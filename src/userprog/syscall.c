b1350002530a390cc2feafb2b218e95f331fd04a
Diego Pontoriero <dmp@cs.stanford.edu>
2012-01-30 15:14:05 -0800

    Added hooks for reading and writing to user memory safely.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,54 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+/* Reads a byte at user virtual address UADDR. UADDR must be below
+   PHYS_BASE.  Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+ 
+/* Writes BYTE to user address UDST. UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
+/* Reads a byte at user virtual address UADDR. Returns the byte value if
+   successful, -1 if address was invalid. */
+static int
+get_byte (const uint8_t *uaddr)
+{
+  if (uaddr < PHYS_BASE)
+    return get_user (uaddr);
+  else
+    return -1;
+}
+
+/* Writes BYTE to user address UDST. Returns true if successful, false if
+   unsuccessful. */
+static bool
+put_byte (const uint8_t *udst, uint8_t byte)
+{
+  if (uaddr < PHYS_BASE)
+    return put_user (udst, byte);
+  else
+    return false;
+}
 
 void
 syscall_init (void) 
