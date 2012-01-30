e43a6aa97498596b918d2671f9008a720540f6c5
Evelyn Gillie <egillie@gmail.com>
2012-01-30 14:56:11 -0800

    changes esp in setup_stack so program won't page fault until argument passing is working

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -28,6 +28,7 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
 tid_t
 process_execute (const char *file_name) 
 {
+  printf("file and args are: %s \n", file_name);
   char *fn_copy;
   tid_t tid;
 
@@ -440,7 +441,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE - 12;
       else
         palloc_free_page (kpage);
     }
