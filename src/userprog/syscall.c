f43f0b1e30d12c519766711e0309bc7e9f94d84a
Peter Thien Tan Pham <ptpham@myth15.Stanford.EDU>
2012-01-30 16:00:24 -0800

    First stab at sys_write

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,17 +4,18 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include <stdint.h>
 
 static void syscall_handler (struct intr_frame *);
 
 static inline void* frame_arg (struct intr_frame *f, int i) 
 {
-  return ((unsigned*)f->esp) + i;
+  return ((uint32_t*)f->esp) + i;
 }
 
-static unsigned get_frame_syscall (struct intr_frame *f) 
+static uint32_t get_frame_syscall (struct intr_frame *f) 
 {
-  return *(unsigned*)frame_arg (f, 0);
+  return *(uint32_t*)frame_arg (f, 0);
 }
 
 /* Reads a byte at user virtual address UADDR. UADDR must be below
@@ -45,7 +46,7 @@ put_user (uint8_t *udst, uint8_t byte)
 static int
 get_byte (const uint8_t *uaddr)
 {
-  if (uaddr < PHYS_BASE)
+  if (((void*)uaddr) < PHYS_BASE)
     return get_user (uaddr);
   else
     return -1;
@@ -54,9 +55,9 @@ get_byte (const uint8_t *uaddr)
 /* Writes BYTE to user address UDST. Returns true if successful, false
    if unsuccessful. */
 static bool
-put_byte (const uint8_t *udst, uint8_t byte)
+put_byte (uint8_t *udst, uint8_t byte)
 {
-  if (uaddr < PHYS_BASE)
+  if ((void*)udst < PHYS_BASE)
     return put_user (udst, byte);
   else
     return false;
@@ -68,28 +69,40 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void 
+
+static uint32_t
 sys_write (struct intr_frame *f) 
 {
   int fd = *(int*)frame_arg (f, 1);
+  uint32_t result = 0;
   if (fd == 1) 
   {
-    void* buffer = *(void**) frame_arg (f, 2);
-    unsigned size = *(unsigned*) frame_arg (3);
-    unsigned i = 0;
+    uint8_t* buffer = *(uint8_t**) frame_arg (f, 2);
+    uint32_t size = *(uint32_t*) frame_arg (f, 3);
+    uint32_t i = 0;
     for (i = 0; i < size; i++) 
     {
-      //TODO: Complete implementation      
+      int byte = get_byte (buffer + i);
+      if (byte == -1) break;
+      
+      // TODO: Write characters to an intermediate buffer for
+      // a single call to putbuf
+      putbuf ((char*)&byte, 1);
+      result++;
     }
   }
+
+  // TODO: Handle other file descriptors
+
+  return result; 
 }
 
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  unsigned syscall = get_frame_syscall (f);
-  
-  printf ("In syscall handlers:\n");
+  uint32_t syscall = get_frame_syscall (f);
+
+  uint32_t eax = f->eax;
 
   switch (syscall) 
   {
@@ -121,7 +134,7 @@ syscall_handler (struct intr_frame *f)
       printf ("Calling SYS_READ, not implemented.\n");
       break;
     case SYS_WRITE:
-      printf ("Calling SYS_WRITE, not implemented.\n");
+      eax = sys_write (f);
       break;
     case SYS_SEEK:
       printf ("Calling SYS_SEEK, not implemented.\n");
@@ -133,4 +146,6 @@ syscall_handler (struct intr_frame *f)
       printf ("Calling SYS_CLOSE, not implemented.\n");
       break;
   }
+
+  f->eax = eax;
 }
