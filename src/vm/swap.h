d404aa9cf729c4cd50b89786c77835005e1bd06d
Diego Pontoriero <dmp@cs.stanford.edu>
2012-02-12 18:57:17 -0800

    skeleton swapping code on paging side. swap table not implemented yet.

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,4 +1,9 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include "devices/block.h"
+
+bool swap_load (uint8_t *dest, block_sector_t swap_block);
+block_sector_t swap_write (uint8_t *src);
+
 #endif /* vm/swap.h */
