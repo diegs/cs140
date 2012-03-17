623d2e637927a02754a0c626dc4f695ff62e9e22
Evelyn Gillie <egillie@gmail.com>
2012-03-17 11:45:36 -0700

    free map locking

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -10,6 +10,7 @@ static struct bitmap *free_map;    /* Free map, one bit per sector. */
 static block_sector_t free_map_begin;
 static block_sector_t free_map_end;
 static block_sector_t root_dir_sector;
+static struct lock free_map_lock;
 
 static bool 
 free_map_write (void)
@@ -53,6 +54,8 @@ free_map_init (void)
 
   /* Set the cwd for the main thread */
   thread_set_cwd (root_dir_sector);
+
+  lock_init (&free_map_lock);
 }
 
 /* Allocates CNT consecutive sectors from the free map and stores
@@ -63,6 +66,7 @@ free_map_init (void)
 bool
 free_map_allocate (size_t cnt, block_sector_t *sectorp)
 {
+  lock_acquire (&free_map_lock);
   block_sector_t sector = bitmap_scan_and_flip (free_map, 0, cnt, false);
   if (sector != BITMAP_ERROR
       && !free_map_write ())
@@ -70,6 +74,8 @@ free_map_allocate (size_t cnt, block_sector_t *sectorp)
     bitmap_set_multiple (free_map, sector, cnt, false); 
     sector = BITMAP_ERROR;
   }
+  lock_release (&free_map_lock);
+
   if (sector != BITMAP_ERROR)
     *sectorp = sector;
   return sector != BITMAP_ERROR;
@@ -80,23 +86,33 @@ void
 free_map_release (block_sector_t sector, size_t cnt)
 {
   ASSERT (bitmap_all (free_map, sector, cnt));
+  lock_acquire (&free_map_lock);
+
   bitmap_set_multiple (free_map, sector, cnt, false);
   free_map_write ();
+
+  lock_release (&free_map_lock);
 }
 
 /* Opens the free map file and reads it from disk. */
 void
 free_map_open (void) 
 {
+  lock_acquire (&free_map_lock);
+
   if (!free_map_read ())
     PANIC ("can't read free map");
+
+  lock_release (&free_map_lock);
 }
 
 /* Writes the free map to disk and closes the free map file. */
 void
 free_map_close (void) 
 {
+  lock_acquire (&free_map_lock);
   free_map_write ();
+  lock_release (&free_map_lock);
 }
 
 /* Creates a new free map file on disk and writes the free map to
@@ -104,7 +120,11 @@ free_map_close (void)
 void
 free_map_create (void) 
 {
+  lock_acquire (&free_map_lock);
+
   /* Write bitmap to file. */
   if (!free_map_write ())
     PANIC ("can't write free map");
+
+  lock_release (&free_map_lock);
 }
